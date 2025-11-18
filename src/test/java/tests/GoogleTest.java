dd2e43c4f1081d0b7a98a530f8b846235e2f3f70
Krystal Om <Krystal.om3@gmail.com>
2025-11-18 15:49:50 -0500

    Update GoogleTest.java

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,8 +1,33 @@
-selenium-sample
-    pom.xml
-    testng.xml
-    src
-        test
-            java
-                tests
-                    GoogleTest.java
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.testng.Assert.assertEquals;
+
+public class GoogleTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkGoogleTitle() {
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        assertEquals(title, "Google");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
